97e46800dc498746a813c77a4f1796e037cb8a7d
KaloyanBP <kaloyanbp2@gmail.com>
2025-07-12 14:08:37 +0300

    Move the accessories into their own controller, create a new Contests controller, Create a new home page with navigation to accessories and the contests pages.

---
+++ DreamAquascape.Web/Controllers/ContestsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DreamAquascape.Web.Controllers
+{
+    public class ContestsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
