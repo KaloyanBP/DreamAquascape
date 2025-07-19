f7efd082c171f0f7b525b09af935f7236780ac91
KaloyanBP <kaloyanbp2@gmail.com>
2025-07-19 09:51:16 +0300

    Added an empty personal contest dashboard controller.

---
+++ DreamAquascape.Web/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DreamAquascape.Web.Controllers
+{
+    /// <summary>
+    /// Personal contest dashboard
+    /// </summary>
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
