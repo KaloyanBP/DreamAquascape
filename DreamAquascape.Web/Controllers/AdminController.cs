951a6b53a58d01b3b42ddc11c16a574e30f94310
KaloyanBP <kaloyanbp2@gmail.com>
2025-07-19 09:51:29 +0300

    Added an empty Admin controller

---
+++ DreamAquascape.Web/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DreamAquascape.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
