7ac4d56dae04a56d223d15c73f2392408e5e0369
KaloyanBP <kaloyanbp2@gmail.com>
2025-07-22 09:38:46 +0300

    Add missing dashboard stat model

---
+++ DreamAquascape.Web.ViewModels/AdminDashboard/DashboardStatsViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace DreamAquascape.Web.ViewModels.AdminDashboard
+{    
+    public class DashboardStatsViewModel
+    {
+        public int TotalContests { get; set; }
+        public int ActiveContests { get; set; }
+        public int TotalEntries { get; set; }
+        public int TotalVotes { get; set; }
+        public int TotalUsers { get; set; }
+        public int PendingEntries { get; set; }
+        public int ContestsEndingSoon { get; set; }
+        public decimal TotalPrizeValue { get; set; }
+
+        // Performance metrics
+        public double AverageEntriesPerContest { get; set; }
+        public double AverageVotesPerEntry { get; set; }
+        public double UserEngagementRate { get; set; }
+    }
+}
