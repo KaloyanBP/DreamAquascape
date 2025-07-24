9e4dcba91326bb9936cb6f73fd89364a83c6e467
KaloyanBP <kaloyanbp2@gmail.com>
2025-07-24 11:03:15 +0300

    Add global DateTime convertion to UTC when store dates to database

--- DreamAquascape.Data/ApplicationDbContext.cs
+++ DreamAquascape.Data/ApplicationDbContext.cs
@@ -3,7 +3,26 @@
     using DreamAquascape.Data.Models;
     using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
     using System.Reflection;
+
+    public class UtcDateTimeConverter : ValueConverter<DateTime, DateTime>
+    {
+        public UtcDateTimeConverter() : base(
+            v => v.ToUniversalTime(),
+            v => DateTime.SpecifyKind(v, DateTimeKind.Utc))
+        {
+        }
+    }
+
+    public class NullableUtcDateTimeConverter : ValueConverter<DateTime?, DateTime?>
+    {
+        public NullableUtcDateTimeConverter() : base(
+            v => v.HasValue ? v.Value.ToUniversalTime() : v,
+            v => v.HasValue ? DateTime.SpecifyKind(v.Value, DateTimeKind.Utc) : v)
+        {
+        }
+    }
 
     public class ApplicationDbContext : IdentityDbContext
     {
@@ -31,6 +50,18 @@
             base.OnModelCreating(builder);
 
             builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            // Global DateTime UTC conversion
+            configurationBuilder
+                .Properties<DateTime>()
+                .HaveConversion<UtcDateTimeConverter>();
+
+            configurationBuilder
+                .Properties<DateTime?>()
+                .HaveConversion<NullableUtcDateTimeConverter>();
         }
     }
 }
